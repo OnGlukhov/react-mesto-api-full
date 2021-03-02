19ca2e8d72d623d12caae15ade3ef61164a4b7fa
Oleg-Glukhov1 <61358701+Oleg-Glukhov1@users.noreply.github.com>
2021-03-02 13:52:47 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { createUser, login } = require('./controllers/users');
 const NotFoundError = require('./errors/NotFoundError');
 
-// Слушаем 3000 порт
-const { PORT = 3000 } = process.env;
+// Слушаем 4000 порт
+const { PORT = 4000 } = process.env;
 
 const app = express();
 // подключаемся к серверу mongo
